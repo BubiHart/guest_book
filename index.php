45a050e752ceca84742e8d41b11086486f3debc3
BubiHart <piskovyi.dmytro@gmail.com>
2017-10-28 16:09:23 +0300

    form appear while clicking on the a tag. making the form content

--- index.php
+++ index.php
@@ -5,13 +5,47 @@
     <head>
       <meta charset="utf-8">
       <title>Guest Book</title>
+      <script type="text/javascript" src="jquery.js" ></script>
       <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.css">
       <link rel="stylesheet" type="text/css" href="style.css">
-      <script type="text/javascript" src="jquery.js" ></script>
       <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.25/jquery.fancybox.min.css">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.25/jquery.fancybox.min.js"></script>
+        <script>
+        $(document).ready(function()
+        {
+      $(".modalbox").fancybox();
+      $('#main_table').dataTable
+      (
+       {
+        "bProcessing": true,
+        "bServerSide": true,
+        "sAjaxSource": "jq_dt_connect.php"
+       }
+      );
+      $("#show_form").on("click", function()
+      {
+        $("#form").css("display", "block");
+
+      }
+     );
+      }
+     );
+       </script>
     </head>
 
     <body>
+      <div id="form">
+        <span>name</span>
+          <input id='name' type='text' />
+        <span>e-mail:</span>
+          <input id='email' type='email' />
+        <span>text:</span>
+          <textarea rows='5' id='text' type='text'></textarea>
+        <span>&nbsp;</span>
+          <input id='submit-id' type='submit' value='Отправить' />
+      </div>
+      <a href="#form" rel="nofollow" class="modalbox">add comment</a>
       <table id="main_table" class="display" width="100%" cellspacing="0">
         <thead>
           <tr>
@@ -31,15 +65,7 @@
         </tbody>
         </table>
     </body>
-    <script>
-    $(document).ready(function() {
-	$('#main_table').dataTable( {
-		"bProcessing": true,
-		"bServerSide": true,
-		"sAjaxSource": "jq_dt_connect.php"
-	} );
-} );
- </script>
+
   </html>
   <?
 ?>
